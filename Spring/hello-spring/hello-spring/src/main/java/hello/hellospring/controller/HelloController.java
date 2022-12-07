59d080b04a8df96d413076962e45f972d3b57c62
leesoyuun <leesoyun4206@naver.com>
2022-12-07 21:28:45 +0900

    Learn mvc pattern of spring

--- Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +11,14 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","hello~!"); //key : data & value : hello
         return "hello"; //여기는 hello.html로 연결
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        //RequestParam : 파라미터 받음
+        //http://portnum/hello-mvc?name=어쩌구
+        //라고 링크 써주어야함
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
