4801bb7566460ddd3e65d4368149b48b4187f76a
leesoyuun <leesoyun4206@naver.com>
2022-12-14 21:19:47 +0900

    Spring basic API

--- Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,5 +21,32 @@ public class HelloController {
         //라고 링크 써주어야함
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    //html 태그 없이 할 수 있도록
+    public String helloString(@RequestParam("name") String name){
+        //http://portnumber/hello-string?name=어저구
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
     }
 }
